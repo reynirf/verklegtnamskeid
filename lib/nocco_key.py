57cfd89360512a3473727eb1918d7b9d6c73c89b
Reynir <reynirfreyr9@gmail.com>
2018-12-04 11:06:03 +0000

    temporary fix unix<->windows bg for NoccoKey

--- lib/nocco_key.py
+++ lib/nocco_key.py
@@ -1,4 +1,4 @@
-import termios
+# import termios
 import sys
 import tty
 
@@ -10,15 +10,19 @@ class NoccoKey:
         return self.getKey()
 
     def get_character(self):
-        fd = sys.stdin.fileno()
-        old_settings = termios.tcgetattr(fd)
         try:
-            tty.setraw(fd)
-            ch = sys.stdin.read(1)
-        finally:
-            termios.tcsetattr(fd, termios.TCSADRAIN,
-                    old_settings)
-            return ch
+            import termios
+            fd = sys.stdin.fileno()
+            old_settings = termios.tcgetattr(fd)
+            try:
+                tty.setraw(fd)
+                ch = sys.stdin.read(1)
+            finally:
+                termios.tcsetattr(fd, termios.TCSADRAIN,
+                        old_settings)
+                return ch
+        except:
+            return '\x1b'
 
     def getKey(self):
         firstChar = self.get_character()
