1b42e83690e4459953090bba292324d917bcf473
ithil13 <ithil13@gmail.com>
2018-12-04 20:53:45 +0000

    added customer repo class and csv file

---
+++ repos/customer_repo.py
@@ -0,0 +1,26 @@
+from models.customer import Customer
+import csv
+
+class CustomerRepo:
+
+    CUSTOMER_FILE = "./data/customers.csv"
+
+    def __init__(self):
+        self.__customer_list = []
+
+    def get_customer_list(self):
+        if self.__customer_list == []:
+            with open(self.CUSTOMER_FILE, 'r') as customer_file:
+                csv_reader = csv.DictReader(customer_file)
+                for line in csv_reader:
+                    customer = Customer(
+                        line['name'],
+                        line['ssn'], 
+                        line['birthday'], 
+                        line['phone'],
+                        line['email'],
+                        line['address'], 
+                        line['driver licence'], 
+                        line['credit card'])
+                    self.__customer_list.append(customer)
+        return self.__customer_list
--- repos/employee_repo.py
+++ repos/employee_repo.py
@@ -6,15 +6,19 @@ class EmployeeRepo:
     EMPLOYEE_FILE = "./data/employees.csv"
 
     def __init__(self):
-        self.employee_list = []
-        self.current_employee = ""
+        self.__employee_list = []
+        self.__current_employee = ""
     
     def get_employee_list(self):
-        if self.employee_list == []:
+        if self.__employee_list == []:
             with open(self.EMPLOYEE_FILE, 'r') as employee_file:
                 csv_reader = csv.DictReader(employee_file)
                 for line in csv_reader:
-                    employee = Employee(line['title'],line['name'], line['id'], line['password'])
-                    self.employee_list.append(employee)
-        return self.employee_list
+                    employee = Employee(
+                        line['title'],
+                        line['name'], 
+                        line['id'], 
+                        line['password'])
+                    self.__employee_list.append(employee)
+        return self.__employee_list
 
