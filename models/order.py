f1c771ae3c9e51669e8471ea7f1de53a658ff84c
ithil13 <ithil13@gmail.com>
2018-12-10 14:33:59 +0000

    testing date formats for orders

--- models/order.py
+++ models/order.py
@@ -1,10 +1,12 @@
+from datetime import date
+
 class Order:
-    def __init__(self, order_id, ssn,
-        start_date, end_date, pick_up_time, return_time, pick_up_location, return_location, number_plate, insurance, vehicle_type):
+    def __init__(self, order_id, ssn, start_date, end_date, 
+            pick_up_time, return_time, pick_up_location, return_location, number_plate, insurance, vehicle_type):
         self.__order_id = order_id
         self.__ssn = ssn
-        self.__start_date = start_date
-        self.__end_date = end_date
+        self.__start_date = date(int(start_date[6:]), int(start_date[3:5]), int(start_date[:2]))
+        self.__end_date = date(int(end_date[6:]), int(end_date[3:5]), int(end_date[:2]))
         self.__pick_up_time = pick_up_time
         self.__return_time = return_time
         self.__pick_up_location = pick_up_location
