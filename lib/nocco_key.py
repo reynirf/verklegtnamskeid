b79511501857feca9f96e06c9f0e4468d3178dea
siggso <siggso@internet.is>
2018-12-11 17:17:58 +0000

    last NoccoKey commit hopefully

--- lib/nocco_key.py
+++ lib/nocco_key.py
@@ -22,40 +22,22 @@ class NoccoKey:
                 termios.tcsetattr(fd, termios.TCSADRAIN,
                                   old_settings)
                 return char
-        except ModuleNotFoundError:
+        except ModuleNotFoundError: # for Windows
             import msvcrt
-            while 1:
-                if msvcrt.kbhit():
-                    msvcrt.getch()
-                    char = msvcrt.getch()
-                    vals = [72, 77, 80, 75]
-                    try:
-                        return vals.index(ord(char.decode('utf-8')))
-                    except ValueError:
-                        return ord(char.decode('utf-8'))
+            firstChar = msvcrt.getch()
+            if firstChar == b'\xe0':
+                return {b'H': 'up', b'P': 'down', b'M': 'right', b'K': 'left'}[msvcrt.getch()]
+            else:
+                return firstChar
 
     def getKey(self):
         firstChar = self.get_character()
         if firstChar == '\x1b':  # looks like this: ^[
             return {
-
                 '[A': 'up',
                 '[B': 'down',
                 '[C': 'right',
                 '[D': 'left',
             }[self.get_character() + self.get_character()]
-        elif os.name == 'nt':
-            if firstChar == 0:
-                return 'up'
-            elif firstChar == 1:
-                return 'right'
-            elif firstChar == 2:
-                return 'down'
-            elif firstChar == 3:
-                return 'left'
-            elif firstChar == 13:
-                return 'enter'
-            else:
-                return firstChar
         else:
             return firstChar
