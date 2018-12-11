2d018ded6d2906f2e0668960c7de599ab794f28f
ithil13 <ithil13@gmail.com>
2018-12-11 22:03:25 +0000

    updated price list

--- models/price.py
+++ models/price.py
@@ -1,11 +1,14 @@
 class Price:
+    def __init__(self):
+        self.__vehicle_types = ['Small car', 'Sedan', 'Offroad', 'Bus']
+
     def get_price(self, vehicle_type):
         if vehicle_type == 'smallcar':
             self.price_per_day = 9000
         elif vehicle_type == 'sedan':
             self.price_per_day = 10000
         elif vehicle_type == 'offroad':
-            self.price_per_day == 12000
+            self.price_per_day = 12000
         else:
             self.price_per_day = 13000
         return self.price_per_day
@@ -20,3 +23,9 @@ class Price:
         else:
             self.extra_insurance = 1500  
         return self.extra_insurance      
+
+    def print_prices(self, vehicle_type):
+        price = self.get_price(vehicle_type)
+        basic_ins = int(price * 0.35)
+        extra_ins = self.get_insurance(vehicle_type)
+        return '{:<15}{:>12}{:>20}{:>20}'.format(vehicle_type, price, basic_ins, extra_ins)
